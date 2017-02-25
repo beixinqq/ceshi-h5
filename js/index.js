aad59ad057c2bd36cbff248593a0aa275021e883
qiubeixin <2533959357@qq.com>
2017-02-25 21:28:43 +0800

    备注

---
+++ js/index.js
@@ -0,0 +1,13 @@
+/**
+ * Created by qiubeixin on 2017/2/25.
+ */
+
+var Myswiper = new Swiper('.swiper-container', {
+    pagination: '.swiper-pagination',
+    paginationClickable: true,
+    spaceBetween: 10,
+    centeredSlides: true,
+    autoplay: 2500,
+    autoplayDisableOnInteraction: false
+});
+
---
+++ js/rem640.js
@@ -0,0 +1,12 @@
+/**
+ * Created by qiubeixin on 2017/2/25.
+ */
+~function () {
+    var desW = 640;
+    var winW = document.documentElement.clientWidth;
+    if (winW > 640) {
+        document.documentElement.style.fontSize='60px';
+        return;
+    };
+    document.documentElement.style.fontSize = winW / desW * 100 + 'px';
+}();
